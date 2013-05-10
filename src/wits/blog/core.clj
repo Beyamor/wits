82d1af8a2549453dbd9173482821dd9e2d1928bb
Beyamor <beyamor@gmail.com>
2013-05-09 17:23:26 -0700

    Reading blogs from files

---
+++ src/wits/blog/core.clj
@@ -0,0 +1,62 @@
+(ns wits.blog.core
+  (:use [clojure.string :only [blank? trim split-lines lower-case]])
+  (:import java.text.SimpleDateFormat))
+
+(defn- parse-params
+  "Taking a sequence of the blog parameter lines,
+   this returns a map of what their specifications."
+  [param-lines]
+  (reduce
+    (fn [result param-line]
+      (let [[_ param-name param-value] (->> param-line
+                                         trim 
+                                         (re-find #"(\w+): +(.*)"))]
+        (case (-> param-name lower-case)
+          "title"
+          (assoc result :title param-value)
+
+          "date"
+          (assoc result :date param-value))))
+    {}
+    param-lines))
+
+(defn parse-blog
+  "Spits out, y'know, contents of a blog file."
+  [raw-blog]
+  (let [lines (-> raw-blog
+                trim
+                split-lines)
+        param-lines (->> lines
+                      (take-while (complement blank?)))
+        contents (->> lines
+                   (drop-while (complement blank?))
+                   (drop-while blank?)
+                   (interpose \newline)
+                   (apply str))
+        {:keys [title date]} (parse-params param-lines)]
+    {:title title
+     :date date
+     :content contents}))
+
+(defn sort-blogs
+  "Sorts a sequence of blogs."
+  [blogs]
+  (let [formatter (SimpleDateFormat. "MM-dd-yyyy")
+        parse-date #(.parse formatter %)]
+    (sort-by
+      #(->> % :date parse-date)
+      #(* -1 (compare %1 %2))
+      blogs)))
+
+(defn load-all-blogs
+  "Loads all of the blogs, obviously. Assumed to  be in /blogs/"
+  []
+  (->>
+    (for [file (-> "./blogs" clojure.java.io/file file-seq)
+          :when (-> file .getName (.endsWith "blarg"))]
+      (->
+        file
+        clojure.java.io/reader
+        slurp
+        parse-blog))
+    sort-blogs))
--- src/wits/blog/pages.clj
+++ src/wits/blog/pages.clj
@@ -1,6 +1,7 @@
 (ns wits.blog.pages
   (:use hiccup.element
-        [hiccup.page :only [html5 include-css]]))
+        [hiccup.page :only [html5 include-css]]
+        [wits.blog.core :only [load-all-blogs]]))
 
 (defn sections
   "Takes section-name/content pairs
@@ -19,18 +20,6 @@
             :date date
             :content content)))
 
-(def blogs
-  [{:title "here's a blog"
-   :date "12-04-2013"
-   :content "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultrices feugiat felis at ornare. Maecenas sagittis sem vel elit tempus sagittis. Proin nibh sapien, elementum ultrices laoreet eget, pretium id sapien. Nullam adipiscing, dui eget semper faucibus, leo magna scelerisque ipsum, id rutrum dui nisi vitae tellus. Morbi nec justo et turpis lacinia bibendum nec ornare arcu. Proin quis eros odio, at consectetur mi. Sed congue congue lorem, ut pharetra urna vulputate eget. Donec in lorem id tortor consequat imperdiet. Quisque vulputate magna quis dui hendrerit suscipit. Quisque molestie leo sit amet massa ultrices vitae ultricies massa viverra.</p>
-
-                                           <p>Integer ac odio sed augue pulvinar blandit non vitae felis. In feugiat dolor at turpis pharetra ut iaculis nisi laoreet. Proin sit amet nisi vitae dolor varius condimentum. Integer in posuere eros. Suspendisse placerat mattis enim non consequat. Duis massa arcu, rutrum ac blandit ac, cursus quis eros. Integer nec urna eget nibh pellentesque faucibus...</p>"}
-  {:title "here's another blog"
-   :date "06-10-2012"
-   :content "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultrices feugiat felis at ornare. Maecenas sagittis sem vel elit tempus sagittis. Proin nibh sapien, elementum ultrices laoreet eget, pretium id sapien. Nullam adipiscing, dui eget semper faucibus, leo magna scelerisque ipsum, id rutrum dui nisi vitae tellus. Morbi nec justo et turpis lacinia bibendum nec ornare arcu. Proin quis eros odio, at consectetur mi. Sed congue congue lorem, ut pharetra urna vulputate eget. Donec in lorem id tortor consequat imperdiet. Quisque vulputate magna quis dui hendrerit suscipit. Quisque molestie leo sit amet massa ultrices vitae ultricies massa viverra.</p>
-
-                                                    <p>Integer ac odio sed augue pulvinar blandit non vitae felis. In feugiat dolor at turpis pharetra ut iaculis nisi laoreet. Proin sit amet nisi vitae dolor varius condimentum. Integer in posuere eros. Suspendisse placerat mattis enim non consequat. Duis massa arcu, rutrum ac blandit ac, cursus quis eros. Integer nec urna eget nibh pellentesque faucibus..."}])
-
 (defn blog-roll
   []
   (html5
@@ -40,5 +29,5 @@
      [:canvas {:id "side-graphic"}]
      (link-to {:id "navigation"} "/" "wits")
      [:div {:id "page-content"}
-      (map truncated-blog blogs)]
+      (map truncated-blog (load-all-blogs))]
      (include-css "/css/blog.css")]))
