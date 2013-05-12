6bbe224c4369b8cf803148c7d0a4a7fe6b94ff52
Beyamor <beyamor@gmail.com>
2013-05-12 15:36:13 -0700

    Pretty substantial restyling

--- src/wits/core/pages.clj
+++ src/wits/core/pages.clj
@@ -22,39 +22,47 @@
       (merge {:id "navigation"} (when additional-classes {:class additional-classes}))
       "/" "wits")))
 
-(defn pjax-page
-  [& {:keys [title content css js script full-page? pjax?]}]
-  (let [title (str "Words in the Sky" (if title (str " - " title)))
-        css (if (and (not full-page?) (not pjax?))
-              (conj css "/css/half-page.css") css)
-        js (concat js ["/js/lib/jquery.pjax.js" "/js/page-content.js"])
-        page (html
-               (when pjax? "<!-- Loaded with PJAX -->")
-               [:div#page-content content]
-               (map include-css css)
-               (map include-js js)
-               (javascript-tag script))]
-    (if pjax?
-      page
-      (html5
-        [:head
-         [:title title]]
-        [:body
-         [:canvas#side-graphic]
-         [:div#page page]
-         (navigation "page-content")]))))
+;
+; I'll be revisiting PJAX after restructuring the site a bit.
+;
+; (defn pjax-page
+;   [& {:keys [title content css js script full-page? pjax?]}]
+;   (let [title (str "Words in the Sky" (if title (str " - " title)))
+;         css (if (and (not full-page?) (not pjax?))
+;               (conj css "/css/half-page.css") css)
+;         js (concat js ["/js/lib/jquery.pjax.js" "/js/page-content.js"])
+;         page (html
+;                (when pjax? "<!-- Loaded with PJAX -->")
+;                [:div#page-content content]
+;                (map include-css css)
+;                (map include-js js)
+;                (javascript-tag script))]
+;     (if pjax?
+;       page
+;       (html5
+;         [:head
+;          [:title title]]
+;         [:body
+;          [:canvas#side-graphic]
+;          [:div#page page]
+;          (navigation "page-content")]))))
 
 (defn regular-page
-  [& {:keys [title content css js script full-page?]}]
-  (let [css (if-not full-page? (conj css "/css/half-page.css") css)]
-    (html
-      [:head
-       [:title (wits-title title)]]
-      [:body
-       (map include-css css)
-       (when-not full-page? [:canvas#side-graphic "Nope."])
-       [:div#page
-        [:div#page-content content]]
-       (navigation)
-       (map include-js js)
-       (if script (javascript-tag script))])))
+  [& {:keys [title content css js script]}]
+  (html
+    [:head
+     [:title (wits-title title)]]
+    [:body
+     (map include-css css)
+     [:canvas#art ""]
+     [:div#page
+      [:div#header
+       [:div.title
+        (link-to "/" "words in the sky")]
+       [:div.navigation
+        (link-to "/blog" "blog")
+        (link-to "/" "code")
+        (link-to "/" "me")]]
+      [:div#page-content content]
+     (map include-js js)
+     (if script (javascript-tag script))]]))
