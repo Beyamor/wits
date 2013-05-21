b9bfc54960d9c9bd6f184ce76e2d533a14ba810c
Beyamor <beyamor@gmail.com>
2013-05-21 11:52:59 -0700

    Displaying nothing for the home page

--- src/wits/handler.clj
+++ src/wits/handler.clj
@@ -7,10 +7,16 @@
             [wits.blog.routes :as blog]
             [wits.code.routes :as code]
             [wits.games.routes :as games]
+            [wits.web.pages :as wits-pages]
+            [wits.web.pjax :as pjax]
             [ring.util.response :as ring-resp]))
 
 (defroutes all-routes
-           (GET "/" [] (ring-resp/redirect "/blog"))
+           (pjax/PJAX "/" []
+                      (pjax/page
+                        wits-pages/main pjax?
+                        {:css
+                         ["/css/home.css"]}))
            blog/all
            code/all
            games/all
