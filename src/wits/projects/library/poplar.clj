8c98ecd4d2d176e6ec884988d45d42b1cba2d273
beyamor <beyamor@gmail.com>
2013-09-18 21:42:40 -0700

    Update poplar description

--- src/wits/projects/library/poplar.clj
+++ src/wits/projects/library/poplar.clj
@@ -12,5 +12,8 @@
   :short-description
   "A prototype for a match-three game mixed with a 2D platformer."
 
+  :description
+  "For some reason, sometimes the first scene doesn't load. If you're seeing a black screen, try pressing space."
+
   :source
   "https://github.com/Beyamor/poplar")
