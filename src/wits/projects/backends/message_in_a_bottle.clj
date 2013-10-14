39574dadae08081d57b0f7b9d942006f350212e4
beyamor <beyamor@gmail.com>
2013-10-14 15:37:27 -0700

    Inserting messages into the database

--- src/wits/projects/backends/message_in_a_bottle.clj
+++ src/wits/projects/backends/message_in_a_bottle.clj
@@ -1,9 +1,38 @@
 (ns wits.projects.backends.message-in-a-bottle
-  (:use [compojure.core :only [GET defroutes]]))
+  (:use [compojure.core :only [GET defroutes]]
+        [wits.data :as data]
+        korma.core
+        [korma.db :only [with-db]]))
+
+(defentity messages
+           (table :miab_messages))
+
+(def all-messages (select* messages))
+
+(defn get-other-messages
+  [ip]
+  (-> all-messages
+    (where (not= ip :ip))
+    select))
+
+(defn insert-message
+  [ip message]
+  (insert messages
+          (values {:ip ip :message message})))
+
+(defn trade-messages
+  [ip message]
+  (with-db data/wits-db
+           (insert-message ip message)
+           (let [other-messages (select all-messages)]
+             (if-not (empty? other-messages)
+               (let [other-message (rand-nth other-messages)]
+                 (:message other-message))
+               "Whoa, no messages"))))
 
 (defroutes all-routes
            (GET "/projects/message-in-a-bottle/submit"
                 {{:keys [message]} :params ip :remote-addr}
-                (if (empty? message)
-                  {:status 400 :body "Dude, I dunno."}
-                  (str ip " - " message))))
+                (if-not (empty? message)
+                  (trade-messages ip message)
+                  {:status 400 :body "Dude, I dunno."})))
