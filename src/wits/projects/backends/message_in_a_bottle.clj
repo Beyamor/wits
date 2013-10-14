c3589bfe7367d6e2ec367803325214fc45d7acc0
beyamor <beyamor@gmail.com>
2013-10-14 15:07:17 -0700

    Handling empty messages

--- src/wits/projects/backends/message_in_a_bottle.clj
+++ src/wits/projects/backends/message_in_a_bottle.clj
@@ -2,5 +2,8 @@
   (:use [compojure.core :only [GET defroutes]]))
 
 (defroutes all-routes
-           (GET "/projects/message-in-a-bottle/submit" {{:keys [message]} :params ip :remote-addr}
-                (str ip " - " message)))
+           (GET "/projects/message-in-a-bottle/submit"
+                {{:keys [message]} :params ip :remote-addr}
+                (if (empty? message)
+                  {:status 400 :body "Dude, I dunno."}
+                  (str ip " - " message))))
