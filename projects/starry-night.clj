c23eebfa746619a686d85c73f6e2f2d94fabc1a8
beyamor <beyamor@gmail.com>
2013-12-20 20:53:47 -0800

    Starry night

---
+++ projects/starry-night.clj
@@ -0,0 +1,12 @@
+{:title
+ "Starry Night"
+
+ :category
+ :art
+
+ :implementation
+ {:type :full-html-page
+  :url "/projects/starry-night/index.html"}
+
+ :description
+ "Press `R`, `Enter`, or `Space` for a new picture"}
