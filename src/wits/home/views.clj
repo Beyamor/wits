faee5b601c3c411b0632b488172bbb3d425835b5
beyamor <beyamor@gmail.com>
2014-01-08 15:47:18 -0800

    Styling for the home page

--- src/wits/home/views.clj
+++ src/wits/home/views.clj
@@ -15,7 +15,7 @@
   Hey there. I'm Tom Gibson.
   
   I'm a software engineer from UVic who gets excited about programming languages, procedural generation, and cardigans.
-  
+
   While you're here, feel free to read my latest rambling over in [the blog](/blog) or check out some of my [projects](/projects). For more of me, hit up my [GitHub](https://github.com/Beyamor) and [Stack Overflow](http://stackoverflow.com/users/1308287/beyamor) accounts.
 
   Got questions or just want to shoot the breeze? Let's talk.")
@@ -26,6 +26,9 @@
     pages/main pjax?
     {:title
      "Words in the Sky"
+
+     :css
+     ["/css/home.css"]
 
      :content
      [:div.page
