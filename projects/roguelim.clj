ed3552f1550e009748dff6a699221f3d9c6085e9
beyamor <beyamor@gmail.com>
2015-03-10 19:53:16 -0700

    Added roguelim

---
+++ projects/roguelim.clj
@@ -0,0 +1,12 @@
+{:title
+ "Roguelim"
+
+  :implementation
+  {:type :source-only
+   :screenshot "/images/projects/roguelim.png"}
+
+  :source
+  "https://github.com/Beyamor/roguelim"
+
+  :description
+  "A roguelike played through Twitter. Check out the (now-defunct) bot [here](https://twitter.com/RogueLim)."}
