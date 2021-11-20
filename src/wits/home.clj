0ae610e01443a97819c66a14176528e8d2488359
Tom Gibson <beyamor@gmail.com>
2021-11-20 11:37:23 -0800

    Changed homepage

--- src/wits/home.clj
+++ src/wits/home.clj
@@ -10,4 +10,4 @@
      :body (md/md-to-html-string
 "Hi! I'm Tom, a terrible machine powered by sweat and anxiety. Sometimes I program!
 
-You can check out [the blog](/blog) for nonsense rambling or [my github](https://github.com/Beyamor) for equally nonsense code.")}))
+You can check out [the blog](/blog) for nonsense rambling or [my github](https://github.com/Beyamor) for equally incoherent code. In theory, you can also find me on [Linkedin](https://www.linkedin.com/in/tom-gibson-60a6b9156/)")}))
