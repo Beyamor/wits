3695b77ce87955d4f1b8e855c7601f8023bc0c08
Tom Gibson <beyamor@gmail.com>
2024-01-14 13:04:42 -0800

    Fix for auth

--- .github/workflows/trigger-site-build.sh
+++ .github/workflows/trigger-site-build.sh
@@ -10,7 +10,6 @@ curl -L \
   -X POST \
   -H 'Accept: application/vnd.github+json' \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
-  -H "Authorization: $PUBLISH_GITHUB_WEBSITE_TOKEN" \
-  -v \
+  -H "Authorization: Bearer $PUBLISH_GITHUB_WEBSITE_TOKEN" \
   'https://api.github.com/repos/Beyamor/beyamor.github.io/dispatches' \
   -d '{"event_type": "test"}'
