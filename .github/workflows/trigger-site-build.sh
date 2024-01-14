554086d899f6d5e92263bb41ab072da36f0feec9
Tom Gibson <beyamor@gmail.com>
2024-01-14 11:46:08 -0800

    Changed execution permission

--- .github/workflows/trigger-site-build.sh
+++ .github/workflows/trigger-site-build.sh
@@ -8,7 +8,7 @@ fi
 
 curl -L \
   -H 'Accept: application/vnd.github+json' \
-  -H "Authorization: $PUBLISH_GITHUB_WEBSITE_TOKEN" \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
+  -H "Authorization: $PUBLISH_GITHUB_WEBSITE_TOKEN" \
   'https://api.github.com/repos/Beyamor/beyamor.github.io/dispatches' \
   -d '{"event_type": "test"}'
